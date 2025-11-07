45e33f8c0d16df8ec2b83e45735f0a33eade5e71
nafila <nafilatk7@gamil.com>
2025-11-07 16:43:23 +0530

    leetcode

--- 9.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(x) {
-  if (x < 0) return false;
-  const str = x.toString();
-  const reversed = str.split('').reverse().join('');
-  return str === reversed;
-}
-console.log(isPalindrome(101));
